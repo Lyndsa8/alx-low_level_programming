9d2f7474f47ff4bdb90e01df916ee7b8ee760cf9
Lyndsa8 <andeyolyndsay02@gmail.com>
2023-08-01 11:33:25 -0700

    At the end of the day, my goal was to be the best hacker

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,3 +1,29 @@
 #include "main.h"
+
+/**
+ * _strncat - function concatenate two strings
+ * using at most n bytes from src
+ * @dest: entered value
+ * @src: entered value
+ * @n: entered value
+ *
+ * Return: dest
+*/
+
 char *_strncat(char *dest, char *src, int n)
-{}
+
+{
+	int i;
+	int j;
+
+	for (j = 0; dest[j] != '\0'; j++)
+	{}
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
+
+	return (dest);
+}
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,3 +1,19 @@
 #include "main.h"
+
+/**
+ * char *_strcpy - program copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string value
+ */
+
 char *_strcpy(char *dest, char *src)
-{}
+{
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
